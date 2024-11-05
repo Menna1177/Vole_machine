4cddf5cafd02df1741835c8394e39059b9d08981
Menna1177 <20231177@stud.fci-cu.edu.eg>
2024-11-05 01:17:54 -0800

    Update Memory.cpp

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,42 +1,43 @@
 // Memory.cpp
 #include "Memory.h"
+using namespace std ;
 
-Memory::Memory(int rows, int cols) : memoryCells(rows, std::vector<std::string>(cols, "00")) {} // Initialize memory cells to "00"
+Memory::Memory(int rows, int cols) : memoryCells(rows, vector<std::string>(cols, "00")) {} // Initialize memory cells to "00"
 
-void Memory::setCell(int row, int col, const std::string& value) {
+void Memory::setCell(int row, int col, const string& value) {
     if (row < 0 || row >= memoryCells.size() || col < 0 || col >= memoryCells[row].size()) {
-        throw std::out_of_range("Invalid memory index.");
+        throw out_of_range("Invalid memory index.");
     }
     memoryCells[row][col] = value; // Set value at the specified location
 }
 
-std::string Memory::getCell(int row, int col) const {
+string Memory::getCell(int row, int col) const {
     if (row < 0 || row >= memoryCells.size() || col < 0 || col >= memoryCells[row].size()) {
-        throw std::out_of_range("Invalid memory index.");
+        throw out_of_range("Invalid memory index.");
     }
     return memoryCells[row][col]; // Get value from the specified location
 }
 
 void Memory::printMemory() const {
-    std::cout << "Memory:\n";
-    std::cout << "    ";
+    cout << "Memory:\n";
+    cout << "    ";
     for (int col = 0; col < memoryCells[0].size(); ++col) {
-        std::cout << std::hex << std::uppercase << std::setw(2) << col << "  "; // Print column headers
+        cout << hex << uppercase << setw(2) << col << "  "; // Print column headers
     }
 
-    std::cout << "\n   ";
+    cout << "\n   ";
     for (int col = 0; col < memoryCells[0].size(); ++col) {
-        std::cout << "----h"; // Print divider
+        cout << "----h"; // Print divider
     }
 
-    std::cout << "\n";
+   cout << "\n";
 
     for (int row = 0; row < memoryCells.size(); ++row) {
-        std::cout << std::hex << std::uppercase << std::setw(2) << row << " | "; // Print row header
+        cout << hex << uppercase << setw(2) << row << " | "; // Print row header
         for (int col = 0; col < memoryCells[row].size(); ++col) {
-            std::cout << memoryCells[row][col] << " ";
-           std:: cout << " ";// Print memory cell value
+            cout << memoryCells[row][col] << " ";
+            cout << " ";// Print memory cell value
         }
-        std::cout << "\n"; // New line after each row
+        cout << "\n"; // New line after each row
     }
 }
