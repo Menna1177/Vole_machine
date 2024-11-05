3708b89ad09334090707ff2d6733bbd65883faa6
nagatrehan <161480842+nagatrehan@users.noreply.github.com>
2024-11-05 15:38:23 +0200

    Update CU.cpp

--- src/CU.cpp
+++ src/CU.cpp
@@ -1,45 +1,34 @@
 #include "CU.h"
 #include "ALU.h"
 #include <stdexcept>
-using namespace std ;
-void CU::execute(const string& opcode, const string& operand, Register& registers, Memory& memory) {
-    int r, s, t, xy;
+#include <iostream>
 
-    if (opcode == "1") { // LOAD from memory to register
+using namespace std;
 
-        r = stoi(operand.substr(0, 1), nullptr, 16);
-        xy = stoi(operand.substr(1), nullptr, 16);
-        if (xy < 256) {
-            registers.setCell(r, memory.getCell(xy / 16, xy % 16));
-        } else {
-            throw out_of_range("Memory address out of bounds for LOAD operation.");
-        }
-    }
-    else if (opcode == "2") { // LOAD immediate value to register
-        r = stoi(operand.substr(0, 1), nullptr, 16);
-        registers.setCell(r, operand.substr(1));
-    }
-    else if (opcode == "3") { // STORE
-        r = stoi(operand.substr(0, 1), nullptr, 16);
-        xy = stoi(operand.substr(1), nullptr, 16);
-        if (xy < 256) {
-            memory.setCell(xy / 16, xy % 16, registers.getCell(r));
-        } else {
-            throw out_of_range("Memory address out of bounds for STORE operation.");
-        }
-    }
-    else if (opcode == "4") { // MOVE
-        r = stoi(operand.substr(0, 1), nullptr, 16);
-        s = stoi(operand.substr(1), nullptr, 16);
-        registers.setCell(s, registers.getCell(r));
-    }
-    else if (opcode == "5") { // ADD (two's complement)
-        r = stoi(operand.substr(0, 1), nullptr, 16);
-        s = stoi(operand.substr(1, 1), nullptr, 16);
-        t = stoi(operand.substr(2, 1), nullptr, 16);
-        registers.setCell(r, ALU::add(registers.getCell(s), registers.getCell(t)));
-    }
-    else if (opcode == "C") { // HALT
-        throw runtime_error("Program halted.");
+void CU::load(int indReg, int memAdd, Register& reg, Memory& mem)
+{
+    reg.setCell(indReg, mem.getCell(memAdd / 16, memAdd % 16));
+}
+
+void CU::load(int indReg, string& content, Register& reg)
+{
+    reg.setCell(indReg, content);
+}
+
+void CU::store(int indReg, int memAdd, Register& reg, Memory& mem)
+{
+    mem.setCell(memAdd / 16, memAdd % 16, reg.getCell(indReg));
+}
+
+void CU::movee(int indReg1, int indReg2, Register& reg)
+{
+    reg.setCell(indReg2, reg.getCell(indReg1));
+}
+
+void CU::jump(int indReg1, int indReg2, Register& reg, int& pc, int targetAdd)
+{
+    if (reg.getCell(indReg1) == reg.getCell(indReg2)){
+        pc = targetAdd;
     }
 }
+
