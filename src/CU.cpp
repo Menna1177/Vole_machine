478a4de4f6e73f1b6a9814dfba31cc855818da5f
Menna1177 <20231177@stud.fci-cu.edu.eg>
2024-11-05 01:14:44 -0800

    Update CU.cpp

--- src/CU.cpp
+++ src/CU.cpp
@@ -1,45 +1,45 @@
 #include "CU.h"
 #include "ALU.h"
 #include <stdexcept>
-
-void CU::execute(const std::string& opcode, const std::string& operand, Register& registers, Memory& memory) {
+using namespace std ;
+void CU::execute(const string& opcode, const string& operand, Register& registers, Memory& memory) {
     int r, s, t, xy;
 
     if (opcode == "1") { // LOAD from memory to register
 
-        r = std::stoi(operand.substr(0, 1), nullptr, 16);
-        xy = std::stoi(operand.substr(1), nullptr, 16);
+        r = stoi(operand.substr(0, 1), nullptr, 16);
+        xy = stoi(operand.substr(1), nullptr, 16);
         if (xy < 256) {
             registers.setCell(r, memory.getCell(xy / 16, xy % 16));
         } else {
-            throw std::out_of_range("Memory address out of bounds for LOAD operation.");
+            throw out_of_range("Memory address out of bounds for LOAD operation.");
         }
     }
     else if (opcode == "2") { // LOAD immediate value to register
-        r = std::stoi(operand.substr(0, 1), nullptr, 16);
+        r = stoi(operand.substr(0, 1), nullptr, 16);
         registers.setCell(r, operand.substr(1));
     }
     else if (opcode == "3") { // STORE
-        r = std::stoi(operand.substr(0, 1), nullptr, 16);
-        xy = std::stoi(operand.substr(1), nullptr, 16);
+        r = stoi(operand.substr(0, 1), nullptr, 16);
+        xy = stoi(operand.substr(1), nullptr, 16);
         if (xy < 256) {
             memory.setCell(xy / 16, xy % 16, registers.getCell(r));
         } else {
-            throw std::out_of_range("Memory address out of bounds for STORE operation.");
+            throw out_of_range("Memory address out of bounds for STORE operation.");
         }
     }
     else if (opcode == "4") { // MOVE
-        r = std::stoi(operand.substr(0, 1), nullptr, 16);
-        s = std::stoi(operand.substr(1), nullptr, 16);
+        r = stoi(operand.substr(0, 1), nullptr, 16);
+        s = stoi(operand.substr(1), nullptr, 16);
         registers.setCell(s, registers.getCell(r));
     }
     else if (opcode == "5") { // ADD (two's complement)
-        r = std::stoi(operand.substr(0, 1), nullptr, 16);
-        s = std::stoi(operand.substr(1, 1), nullptr, 16);
-        t = std::stoi(operand.substr(2, 1), nullptr, 16);
+        r = stoi(operand.substr(0, 1), nullptr, 16);
+        s = stoi(operand.substr(1, 1), nullptr, 16);
+        t = stoi(operand.substr(2, 1), nullptr, 16);
         registers.setCell(r, ALU::add(registers.getCell(s), registers.getCell(t)));
     }
     else if (opcode == "C") { // HALT
-        throw std::runtime_error("Program halted.");
+        throw runtime_error("Program halted.");
     }
 }
