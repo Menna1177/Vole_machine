2066bcb13e514e6ce4a8ae8af575c35422c9d1d2
Menna1177 <20231177@stud.fci-cu.edu.eg>
2024-11-05 01:18:24 -0800

    Update Register.cpp

--- src/Register.cpp
+++ src/Register.cpp
@@ -1,26 +1,26 @@
 // Registers.cpp
 #include "Register.h"
-
+using namespace std ;
 Register::Register() : registerValues(16, "00") {} // Initialize all registers to "00"
 
 void Register::printRegisters() const {
-    std::cout << "Registers:\n";
+    cout << "Registers:\n";
     for (int i = 0; i < registerValues.size(); i++) {
-        std::cout << "R" << std::hex << std::uppercase << std::setw(2) << i << ": "
+        cout << "R" << hex << uppercase << setw(2) << i << ": "
                   << registerValues[i] << "\n";
     }
 }
 
-std::string Register::getCell(int index) const {
+string Register::getCell(int index) const {
     if (index < 0 || index >= registerValues.size()) {
-        throw std::out_of_range("Invalid register index.");
+        throw out_of_range("Invalid register index.");
     }
     return registerValues[index];
 }
 
-void Register::setCell(int index, const std::string& value) {
+void Register::setCell(int index, const string& value) {
     if (index < 0 || index >= registerValues.size()) {
-        throw std::out_of_range("Invalid register index.");
+        throw out_of_range("Invalid register index.");
     }
     registerValues[index] = value;
 }
