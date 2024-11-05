0126239b7b53c827a56b342a612d53df890bd587
nagatrehan <161480842+nagatrehan@users.noreply.github.com>
2024-11-05 15:42:25 +0200

    Update Memory.cpp

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,43 +1,16 @@
-// Memory.cpp
 #include "Memory.h"
-using namespace std ;
+#include <iostream>
+#include <iomanip>
 
-Memory::Memory(int rows, int cols) : memoryCells(rows, vector<string>(cols, "00")) {} // Initialize memory cells to "00"
+using namespace std;
 
-void Memory::setCell(int row, int col, const string& value) {
-    if (row < 0 || row >= memoryCells.size() || col < 0 || col >= memoryCells[row].size()) {
-        throw out_of_range("Invalid memory index.");
-    }
-    memoryCells[row][col] = value; // Set value at the specified location
+Memory::Memory(int r, int c) : memory(r, vector<string>(c, "00")){}
+
+string Memory::getCell(int row, int col) {
+    return memory[row][col];
 }
 
-string Memory::getCell(int row, int col) const {
-    if (row < 0 || row >= memoryCells.size() || col < 0 || col >= memoryCells[row].size()) {
-        throw out_of_range("Invalid memory index.");
-    }
-    return memoryCells[row][col]; // Get value from the specified location
+void Memory::setCell(int row, int col, const string& val) {
+    memory[row][col] = val;
 }
 
-void Memory::printMemory() const {
-    cout << "Memory:\n";
-    cout << "    ";
-    for (int col = 0; col < memoryCells[0].size(); ++col) {
-        cout << hex << uppercase << setw(2) << col << "  "; // Print column headers
-    }
-
-    cout << "\n   ";
-    for (int col = 0; col < memoryCells[0].size(); ++col) {
-        cout << "----"; // Print divider
-    }
-
-   cout << "\n";
-
-    for (int row = 0; row < memoryCells.size(); ++row) {
-        cout << hex << uppercase << setw(2) << row << " | "; // Print row header
-        for (int col = 0; col < memoryCells[row].size(); ++col) {
-            cout << memoryCells[row][col] << " ";
-            cout << " ";// Print memory cell value
-        }
-        cout << "\n"; // New line after each row
-    }
-}
