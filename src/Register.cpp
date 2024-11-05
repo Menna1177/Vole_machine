b59bb3f8cef10c9869532bf799974e46c462127a
nagatrehan <161480842+nagatrehan@users.noreply.github.com>
2024-11-05 15:42:52 +0200

    Update Register.cpp

--- src/Register.cpp
+++ src/Register.cpp
@@ -1,26 +1,17 @@
-// Registers.cpp
 #include "Register.h"
-using namespace std ;
-Register::Register() : registerValues(16, "00") {} // Initialize all registers to "00"
+#include "ALU.h"
+#include <iostream>
 
-void Register::printRegisters() const {
-    cout << "Registers:\n";
-    for (int i = 0; i < registerValues.size(); i++) {
-        cout << "R" << hex << uppercase << setw(2) << i << ": "
-                  << registerValues[i] << "\n";
-    }
+using namespace std;
+
+Register::Register(int sz) : registers(sz, "00"){}
+
+string Register::getCell(int ind)
+{
+    return registers[ind];
 }
 
-string Register::getCell(int index) const {
-    if (index < 0 || index >= registerValues.size()) {
-        throw out_of_range("Invalid register index.");
-    }
-    return registerValues[index];
-}
-
-void Register::setCell(int index, const string& value) {
-    if (index < 0 || index >= registerValues.size()) {
-        throw out_of_range("Invalid register index.");
-    }
-    registerValues[index] = value;
+void Register::setCell(int ind, const string& val)
+{
+    registers[ind] = val;
 }
