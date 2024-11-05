294e5b83d3ab9a056101e1f9ed12f174b53083cf
Menna1177 <20231177@stud.fci-cu.edu.eg>
2024-11-05 01:20:23 -0800

    Update Memory.h

--- include/Memory.h
+++ include/Memory.h
@@ -5,16 +5,17 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+using namespace std ;
 
 class Memory {
 public:
     Memory(int rows, int cols); // Constructor to initialize memory
-    void setCell(int row, int col, const std::string& value); // Set value at specific location
-    std::string getCell(int row, int col) const; // Get value at specific location
+    void setCell(int row, int col, const string& value); // Set value at specific location
+    string getCell(int row, int col) const; // Get value at specific location
     void printMemory() const; // Method to print the memory
 
 private:
-    std::vector<std::vector<std::string>> memoryCells; // 2D vector to hold memory cells
+    vector<vector<string>> memoryCells; // 2D vector to hold memory cells
 };
 
 #endif // MEMORY_H
