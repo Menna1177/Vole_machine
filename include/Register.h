aba4ac8773610e5903208ce8286dd9a777189248
nagatrehan <161480842+nagatrehan@users.noreply.github.com>
2024-11-05 15:37:26 +0200

    Update Register.h

--- include/Register.h
+++ include/Register.h
@@ -1,20 +1,20 @@
-// Registers.h
-#ifndef REGISTERS_H
-#define REGISTERS_H
+#ifndef REGISTER_H
+#define REGISTER_H
 
 #include <iostream>
 #include <vector>
-#include <iomanip>
-using namespace std ;
-class Register {
-public:
-    Register(); // Constructor
-    void printRegisters() const; // Method to print registers
-    string getCell(int index) const; // Method to get value of a register
-    void setCell(int index, const string& value); // Method to set value of a register
 
-private:
-    vector<string> registerValues; // Vector to hold register values
+using namespace std;
+
+class Register
+{
+    public:
+        Register(int sz = 16);
+        string getCell(int ind);
+        void setCell(int ind, const string& val);
+
+    private:
+        vector<string> registers;
 };
 
-#endif // REGISTERS_H
+#endif // REGISTER_H
