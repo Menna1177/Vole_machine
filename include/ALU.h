769352324e78790e738e3f2ef583e3ae83c930f6
nagatrehan <161480842+nagatrehan@users.noreply.github.com>
2024-11-06 20:07:46 +0200

    Update ALU.h

--- include/ALU.h
+++ include/ALU.h
@@ -1,15 +1,32 @@
 #ifndef ALU_H
 #define ALU_H
 
-#include <iostream>
-
+#include <string>
+#include <map>
+#include <bitset>
 using namespace std;
-
 class ALU {
 public:
-    static string add(const string& reg1, const string& reg2);
+    ALU(); 
+    ~ALU(); 
 
-    static string toHex(int val);
+    string normalizeBinary(string& M3);
+    string binToHex(const string& binary);
+    string hexToBin(const string& hex);
+    double binToDec(const string& binary);
+    string intToBin(int integer);
+    string fracToBin(float fraction);
+    string deciToBin(float decimal);
+    int twosComplementToDecimal(const string& binary);
+    string decimalToTwosComplement(int decimal);
+    string addFloat(string hexa1, string hexa2);
+    string addInteger(string hexa1, string hexa2);
+    string hexToTwosComp(string hexa);
+    string OR(string hexa1, string hexa2);
+    string AND(string hexa1, string hexa2);
+    string XOR(string hexa1, string hexa2);
+    string Rotate(string hexa, int t);
+
 };
 
 #endif // ALU_H
