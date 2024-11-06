128a883bd91a62e65d168c6918c58941f880431c
habiba-264 <163057041+habiba-264@users.noreply.github.com>
2024-11-07 01:28:19 +0200

    Update ALU.h

--- include/ALU.h
+++ include/ALU.h
@@ -1,27 +1,30 @@
+
+
 #ifndef ALU_H
 #define ALU_H
 
 #include <string>
 #include <map>
 #include <bitset>
+#include <iostream>
 using namespace std;
 class ALU {
 public:
-    ALU(); 
-    ~ALU(); 
-
-    string normalizeBinary(string& M3);
+    ALU(); // Constructor
+    ~ALU(); // Destructor
+    int shiftCounter;
+    string sumToBin(float decimal);
+    float get_decimal_by3parts(string binary);
+    string normalizeBinary(string M);
     string binToHex(const string& binary);
     string hexToBin(const string& hex);
     double binToDec(const string& binary);
-    string intToBin(int integer);
-    string fracToBin(float fraction);
-    string deciToBin(float decimal);
+    string decToBin(float decimal);
     int twosComplementToDecimal(const string& binary);
     string decimalToTwosComplement(int decimal);
     string addFloat(string hexa1, string hexa2);
     string addInteger(string hexa1, string hexa2);
-    string hexToTwosComp(string hexa);
+     string hexToTwosComp(string hexa);
     string OR(string hexa1, string hexa2);
     string AND(string hexa1, string hexa2);
     string XOR(string hexa1, string hexa2);
